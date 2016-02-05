18ea13cf49cb879df09967f1f58503ea7624d5e6
Leonard Gonzalez <webbizgroup@Mac-mini-de-Webbiz-2.local>
2016-02-05 16:23:04 -0430

    Manejo de rutas basicas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,20 @@
 |
 */
 
+Route::get('prueba', function(){
+    return "Hola desde routes.php";
+});
+
+/* Rutas con parametros*/
+Route::get('nombre/{nombre}', function($nombre){
+    return "Mi nombre es: ".$nombre;
+});
+
+/* Rutas con parametros y parametro asignado*/
+Route::get('edad/{edad vc ffcv f  fcv bfcv bbfcv b}', function(edad = 20){
+    return "Mi edad es: ".edad;
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
---
+++ git-cmd.md
@@ -0,0 +1,11 @@
+## Crear rama 
+git checkout -b nombre_rama
+
+## Agregar los cambios 
+git add .
+
+## Agregar commit y comentario
+git commit -m "Mensaje..."
+
+## Subir rama al repositorio
+git push origin nombre_rama
